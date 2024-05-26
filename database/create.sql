7288df1454a4a6bc4bdd0355df8bf1b20d80854d
lucasmsouza24 <lucasmesquita58@hotmail.com>
2024-05-26 20:49:24 -0300

    feat(development environment): :sparkles: creating view

--- database/create.sql
+++ database/create.sql
@@ -37,22 +37,25 @@ CREATE TABLE IF NOT EXISTS `tb_modelo` (
 	PRIMARY KEY (`id_modelo`)
 );
 
-CREATE TABLE IF NOT EXISTS `vw_modelo_desempenho` (
-	`id_modelo` int NOT NULL,
-	`id_execucao` int NOT NULL,
-	`id_parametro` int NOT NULL,
-	`id_desempenho` int NOT NULL,
-	`nome_modelo` varchar(255) NOT NULL,
-	`nome_parametro` varchar(255) NOT NULL,
-	`tp_classe` int NOT NULL,
-	`vl_accuracy` decimal(10,3) NOT NULL,
-	`vl_parametro` decimal(10,3) NOT NULL,
-	`vl_precision` decimal(10,3) NOT NULL,
-	`vl_recall` decimal(10,3) NOT NULL,
-	`vl_f1_score` decimal(10,3) NOT NULL,
-	`dt_inicio_exec` datetime NOT NULL,
-	`dt_fim_exec` datetime NOT NULL
-);
+CREATE OR REPLACE VIEW vw_modelo_desempenho AS
+SELECT
+a.id_modelo,
+b.id_execucao,
+a.nome_modelo,
+a.nome_base,
+c.nome_parametro,
+c.vl_parametro,
+b.dt_inicio_exec,
+b.dt_fim_exec,
+b.vl_accuracy,
+d.tp_classe,
+d.vl_precision,
+d.vl_recall,
+d.vl_f1_score
+FROM tb_modelo AS a
+LEFT JOIN tb_execucao AS b ON a.id_modelo = b.fk_modelo
+LEFT JOIN tb_hiperparametro AS c ON c.fk_execucao = b.id_execucao
+LEFT JOIN tb_desempenho AS d ON d.fk_execucao = b.id_execucao;
 
 ALTER TABLE `tb_desempenho` ADD CONSTRAINT `tb_desempenho_fk1` FOREIGN KEY (`fk_execucao`) REFERENCES `tb_execucao`(`id_execucao`);
 ALTER TABLE `tb_execucao` ADD CONSTRAINT `tb_execucao_fk1` FOREIGN KEY (`fk_modelo`) REFERENCES `tb_modelo`(`id_modelo`);
