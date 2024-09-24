7375cf99c909f3a46cccadae551cbb4ddddda4e4
erikspacheco <erik.pacheco@outlook.com>
2024-09-23 22:14:10 -0300

    Alterção no arquivo main do lambda para teste de extração e carregamento

--- Lambda Codes/raw_trusted/main.py
+++ Lambda Codes/raw_trusted/main.py
@@ -1,83 +1,35 @@
 import os
 import json
 import awswrangler as wr
-from sklearn.preprocessing import LabelEncoder
 
-def lambda_handler(event, context):  # sourcery skip: extract-method
-    try: 
-        print(f"Evento {event}")
+def lambda_handler(event, context):
 
-        bucket_raw = os.environ['BUCKET_RAW']
-        bucket_trusted = os.environ['BUCKET_TRUSTED']
+    print(f"Evento {event}")
 
-        message = json.loads(json.loads(event['Records'][0]['body'])['Message'])
+    bucket_raw = os.environ['BUCKET_RAW']
+    bucket_trusted = os.environ['BUCKET_TRUSTED']
 
-        if 'Records' in message.keys():
-            object_key = message['Records'][0]['s3']['object']['key']
-            path_raw = f"s3://{bucket_raw}/{object_key}"
+    message = json.loads(json.loads(event['Records'][0]['body'])['Message'])
 
-            print(f'{path_raw = }')
+    if 'Records' in message.keys():
 
-            # Lê o arquivo CSV do S3 raw
-            df = wr.s3.read_csv(path=path_raw)
+        object_key = message['Records'][0]['s3']['object']['key']
 
-            df_tratado = apply_all_rules(df)
+        path_raw = f"s3://{bucket_raw}/{object_key}"
 
-            # Define o caminho para gravar o CSV no S3 trusted
-            path_trusted = f's3://{bucket_trusted}/{object_key}'
+        print(f'{path_raw = }')
 
-            print(f'write s3: {path_trusted = }')
+        # Lê o arquivo CSV do S3 raw
+        df = wr.s3.read_csv(path=path_raw)
 
-            # Grava o DataFrame como CSV no S3 trusted
-            wr.s3.to_csv(
-                df=df_tratado,
-                path=path_trusted,
-                index=False
-            )
+        # Define o caminho para gravar o CSV no S3 trusted
+        path_trusted = f's3://{bucket_trusted}/{object_key}'
 
-    except Exception as e:
-        print(f"Erro durante a execução da função: {str(e)}")
-        raise
+        print(f'write s3: {path_trusted = }')
 
-# Removendo IDs
-def remove_ids(df):
-    # Identificar colunas com apenas valores únicos (como IDs)
-    colunas_unicas = [coluna for coluna in df.columns if df[coluna].nunique() == len(df)]
-
-    # Remover as colunas com valores únicos
-    dados_processados = df.drop(columns=colunas_unicas)
-    return dados_processados
-
-# Removendo colunas muito variadas (Descrições, Nomes)
-def remove_variable_columns(df):
-    # Definir o limiar para o número de valores únicos em relação ao tamanho do conjunto de dados
-    limiar_valores_unicos = 0.7  # Por exemplo, 50% dos valores únicos
-
-    # Identificar colunas descritivas com muitos valores únicos
-    colunas_descritivas = [coluna for coluna in df.columns if df[coluna].nunique() / len(df) > limiar_valores_unicos]
-
-    # Remover as colunas descritivas
-    dados_processados = df.drop(columns=colunas_descritivas)
-    return dados_processados
-
-# Ajustando valores categóricos
-def adjust_categoric_values(df):
-    
-    # Selecionar dinamicamente as colunas categóricas
-    colunas_categoricas = df.select_dtypes(include=['object']).columns
-
-    # Inicializar o LabelEncoder
-    label_encoder = LabelEncoder()
-
-    # Iterar sobre as colunas categóricas e aplicar LabelEncoder em cada uma
-    for coluna in colunas_categoricas:
-        df[coluna] = label_encoder.fit_transform(df[coluna])
-    
-    return df
-
-# Aplicando todas as regras
-def apply_all_rules(df):
-    df = remove_ids(df)
-    df = remove_variable_columns(df)
-    df = adjust_categoric_values(df)
-    return df
+        # Grava o DataFrame como CSV no S3 trusted
+        wr.s3.to_csv(
+            df=df,
+            path=path_trusted,
+            index=False
+        )
