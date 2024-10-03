29195ead731bd11b485d8bd02e6350624028a5f5
erikspacheco <erik.pacheco@outlook.com>
2024-10-02 23:53:57 -0300

    fix: :bug: Separação do .sh em dois. Um cria um usuário e o outro configura o jupyter

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -1,34 +1,18 @@
 #!/bin/bash
 sudo apt update -y
 sudo apt upgrade -y
-sudo apt-get install python3 -y
-sudo apt-get install python3-pip -y
-sudo apt-get install python3-venv -y
 
-sudo python3 -m venv pyenv
-source pyenv/bin/activate
+# Nome do novo usuário
+NOVO_USUARIO="urubu100"
 
-sudo pip3 install --upgrade --force-reinstall jupyterlab
-sudo pip3 install notebook
-sudo pip3 install qtconsole
-sudo pip3 install pandas
-sudo pip3 install scikit-learn
-sudo pip3 install mysql-connector-python
-sudo pip3 install boto3
-sudo pip3 install numpy
+# Adicionar o novo usuário sem senha
+sudo adduser $NOVO_USUARIO --gecos "" --disabled-password
 
-sudo echo 'export PATH="$PATH:/usr/local/bin"' >> ~/.bashrc
-source ~/.bashrc
-source pyenv/bin/activate
+# Adicionar o novo usuário ao grupo sudo
+sudo usermod -aG sudo $NOVO_USUARIO
 
-# Gera o arquivo de configuração do Jupyter
-jupyter notebook --generate-config
+# Configurar o novo usuário para não precisar de senha para sudo
+echo "$NOVO_USUARIO ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$NOVO_USUARIO
 
-# Adiciona configurações para desabilitar autenticação por senha
-echo "c.NotebookApp.token = ''" >> ~/.jupyter/jupyter_notebook_config.py
-echo "c.NotebookApp.password = ''" >> ~/.jupyter/jupyter_notebook_config.py
-
-git clone https://github.com/Grupo-09-2024-SPTECH/artificial-intelligence.git
-
-# Inicia o Jupyter Lab sem autenticação
-jupyter-lab --ip 0.0.0.0 --allow-root
+# Trocar para o novo usuário sem solicitar senha
+sudo su - $NOVO_USUARIO
--- dependencies/install_2.sh
+++ dependencies/install_2.sh
@@ -2,21 +2,6 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-# Nome do novo usuário
-NOVO_USUARIO="urubu100"
-
-# Adicionar o novo usuário sem senha
-sudo adduser $NOVO_USUARIO --gecos "" --disabled-password
-
-# Adicionar o novo usuário ao grupo sudo
-sudo usermod -aG sudo $NOVO_USUARIO
-
-# Configurar o novo usuário para não precisar de senha para sudo
-echo "$NOVO_USUARIO ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$NOVO_USUARIO
-
-# Trocar para o novo usuário sem solicitar senha
-sudo su - $NOVO_USUARIO
-
 sudo apt-get install python3 -y
 sudo apt-get install python3-pip -y
 sudo apt-get install python3-venv -y
